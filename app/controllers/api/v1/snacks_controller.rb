cd702fd5b8e6714a8d1d1d724ff07df394dc2d36
Jenna Ransom <jenna.ransom@gmail.com>
2020-06-13 17:27:32 -0600

    namespace routes and controllers

---
+++ app/controllers/api/v1/snacks_controller.rb
@@ -0,0 +1,2 @@
+class Api::V1::SnacksController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  resources :reviews
-  resources :snacks
-  resources :users
+  namespace :api do 
+    namespace :v1 do 
+      resources :reviews
+      resources :snacks
+      resources :users
+    end
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
