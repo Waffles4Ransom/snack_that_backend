c5820c31d97139bf0959c011a0d14208f7991fc3
Jenna Ransom <jenna.ransom@gmail.com>
2020-06-14 14:30:30 -0600

    add more user controller actions and strong params

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,7 +2,32 @@ class Api::V1::UsersController < ApplicationController
 
   def index
     users = User.all
-    render json: users
+    render json: users, except: [:created_at, :updated_at, :password_digest]
   end 
-  
+
+  def show
+    user = User.find_by(id: params[:id])
+    render json: user, only: [:name, :username, :location, :profile_photo], include: :snacks
+  end 
+
+  def create
+    @user = User.new(user_params)
+    if @user.save 
+      # log_in(@user)
+      render json: @user, only: [:username, :name, :location, :profile_photo]
+    else
+      render json: { errors: user.errors.full_messages }
+    end
+  end 
+
+  def update
+
+  end 
+
+  private 
+
+  def user_params 
+    params.require(:user).permit(:username, :password_digest, :name, :location, :profile_photo)
+  end 
+
 end
